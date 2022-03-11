5987cc793fc1c6d61f4923f42177e7f43475d3fc
Gustavo Tomás <guga.tomas10@gmail.com>
2022-03-11 18:54:50 -0300

    Teste

--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-
-
+    for (int i = 0; i < 10; i++) {
+        cout << "Luiz nunca diz não, ";
+        cout << "mas Rafael sempre diz sim" << endl;
+    }
     return 0;
 }
