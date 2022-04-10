f770b5ab48f1cc975485d59704c2edca5b3af3bd
Gustavo Tomás <guga.tomas10@gmail.com>
2022-04-09 22:16:15 -0300

    started reference type v2

---
+++ include/references.hpp
@@ -0,0 +1,20 @@
+#ifndef _REFERENCES_HPP
+#define _REFERENCES_HPP
+
+#include "types.hpp"
+#include <vector>
+
+using namespace std;
+
+struct array_t
+{
+    int type;
+    u4 max_size;
+    vector<void*> items;
+
+    array_t(u4 max_size) : max_size(max_size) {};
+    void insert(u4 index);
+    void* operator[](u4 index) { return items[index]; }
+};
+
+#endif // _REFERENCES_HPP
